8b47a4a77ab534440d01795d80f18c3d5349a1a9
nuran-o <92818508+nuran-o@users.noreply.github.com>
2021-11-28 16:33:29 +0000

    Update utils.py
    Actioning Lou's comments to add try-catch for API connections. and if__main__ statement. Note: This try-catch checks if the API request connection was made successfully, it does not catch for rand_character() if the authentication access_token is not correct - not sure if we need to/ how we would capture that?

--- utils.py
+++ utils.py
@@ -3,12 +3,16 @@ import requests
 import random
 
 def random_insult():
-     url="https://evilinsult.com/generate_insult.php?lang=en&type=json"
-     request = requests.get(url)
-     request = request.json()
-     return(request['insult'])
+     try:
+          url="https://evilinsult.com/generate_insult.php?lang=en&type=json"
+          request = requests.get(url)
+     except:
+          raise ConnectionError(f"Unable to connect to api {url}.")
+     else:
+          request = request.json()
+          return(request['insult'])
 
-random_insult()
+
 
 #To run rand_character() you need to go to https://superheroapi.com/ and get an access token. Put that into the access_token Variable e.g.
 #The below code calls the full json for the random superhero selected
@@ -17,8 +21,14 @@ def rand_character():
      rand_gen=str(random.randint(1,731))
      access_token="YOUR GENERATED ACCESS CODE GOES HERE"
      url=(f"https://superheroapi.com/api/{access_token}/{rand_gen}")
-     request = requests.get(url)
-     request = request.json()
-     return(request)
+     try:
+          request = requests.get(url)
+     except:
+          raise ConnectionError(f"Unable to connect to api {url}.")
+     else:
+          request = request.json()
+          return(request)
 
-rand_character()
+if __name__ == '__main__':
+     random_insult()
+     rand_character()
