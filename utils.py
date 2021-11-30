cbe481820d49b30fd9787c32acdc26afe4ac5a60
nuran-o <92818508+nuran-o@users.noreply.github.com>
2021-11-30 20:36:16 +0000

    Update utils.py
    Initial draft of code to set user-character - having trouble breaking out of while loop and returning correct_selection

--- utils.py
+++ utils.py
@@ -25,3 +25,33 @@ def get_character(num):
     else:
         request = request.json()
         return (request)
+
+def set_user_character():
+    player_options = {
+        1: {'name': 'Buffy',
+            'id': 140},
+        2: {'name': 'Deadpool',
+            'id': 213},
+        3: {'name': 'Mystique',
+            'id': 480},
+        4: {'name': 'Rambo',
+            'id': 540}
+        }
+
+    player_select = int(input(f"Which player would you like to select? Enter: \n1 for Buffy\n2 for Deadpool\n3 for Mystique\n4 for Rambo \nEnter your selection now: "))
+    if player_select not in [1,2,3,4]:
+        print("Oh dear, that is not one of our options. Let's try again.\n")
+        set_user_character()
+    selection=player_options[player_select]["name"]
+    correct_selection = input(f"Is {selection} who you wish to play? y/n: ")
+    while correct_selection == "n":
+        for option in player_options.values():
+            correct_selection = input(f"Is {option['name']} who you wish to play? y/n: ")
+            if correct_selection == "y":
+                return correct_selection
+        return correct_selection #HOW DO I RETURN CORRECT_SELECTION TO CONT BELOW?
+    if correct_selection == "y":
+        id=player_options[player_select]["id"]
+        print(id)
+
+set_user_character()
