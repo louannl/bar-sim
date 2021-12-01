46a3a7480080bdd6ada68a6672ec3ada577f566b
Zoe Thomas <zoethomas2893@gmail.com>
2021-12-01 20:42:03 +0000

    dice func updates

--- dice_func.py
+++ dice_func.py
@@ -1,44 +1,40 @@
 # simple dice function
 
+import random
+
 
 class DiceDecider:
 
     def __init__(self):
         self.action = None
         self.dice_result = None
+        self.count_of_tries = None
 
+    # self explanatory! its a simple dice
     def dice_roll(self):
-        import random
         self.dice_result = random.randint(1, 6)
         return self.dice_result
 
+    # I got rid of 50/50 odds bc it was just this with more input options
     def is_even(self):
         self.dice_roll()
         return (self.dice_result % 2) == 0
 
-    def try_action_50_50(self, action):
-        if self.is_even():
-            print("Success! You have managed to {}".format(action))
-            return True
+    # this can be changed if we want to fix a number of retries, it also doesn't print every outcome
+    def retry_action(self, func, max_retries):
+        self.count_of_tries = 0
+        while self.count_of_tries != max_retries:
+            return func
         else:
-            try_new_thing = input("Oh no! You failed to {}, would you like to try something else? y/n ".format(action))
-            if try_new_thing == 'y':
-                action = input("What would you like to try instead? ")
-                self.try_action_50_50(action)
-            else:
-                exit()
             return False
 
-    def try_action_custom_odds(self, action, min_num_to_do_action):
-        dice_roll = self.dice_roll()
-        if dice_roll <= min_num_to_do_action:
-            print("Success! You have managed to {}".format(action))
-            return True
+    # this is for the pint element!
+    def pint_increase_decider(self):
+        if self.dice_roll() <= 2:
+            return 1
+        elif self.dice_roll() < 5:
+            return 2
         else:
-            try_new_thing = input("Oh no! You failed to {}, would you like to try something else? y/n ".format(action))
-            if try_new_thing == 'y':
-                action = input("What would you like to try instead? ")
-                self.try_action_custom_odds(action, min_num_to_do_action)
-            else:
-                exit()
-            return False
+            return 3
+
+
