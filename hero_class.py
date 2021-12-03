4947eaab8504158950f3d41c6ff323821c035f06
nuran-o <nuran.a.ozan@gmail.com>
2021-12-03 05:06:01 +0000

    Update hero_class.py
    removing the infinite loop - now sorted :-)

--- hero_class.py
+++ hero_class.py
@@ -14,7 +14,7 @@ class Superhero():
         self.height=request["appearance"]["height"][0] #returns height in feet.
 
     def getName(self):
-        return self.getName()
+        return self.name()
 
 
 #EXAMPLE CALL TO CREATE USER CHARACTER
