c25b25148f20d0fd42e1b79a0edcc454ce344c88
Zoe Thomas <zoethomas2893@gmail.com>
2021-12-04 16:42:41 +0000

    updates

--- utils/dice_func.py
+++ utils/dice_func.py
@@ -6,9 +6,7 @@ import random
 class DiceDecider:
 
     def __init__(self):
-        self.action = None
         self.dice_result = None
-        self.count_of_tries = None
 
     # self explanatory! its a simple dice
     def dice_roll(self):
@@ -20,19 +18,12 @@ class DiceDecider:
         self.dice_roll()
         return (self.dice_result % 2) == 0
 
-    # this can be changed if we want to fix a number of retries, it also doesn't print every outcome
-    def retry_action(self, func, max_retries):
-        self.count_of_tries = 0
-        while self.count_of_tries != max_retries:
-            return func
-        else:
-            return False
-
     # this is for the pint element!
     def pint_increase_decider(self):
-        if self.dice_roll() <= 2:
+        self.dice_roll()
+        if self.dice_result <= 2:
             return 1
-        elif self.dice_roll() < 5:
+        elif self.dice_result < 5:
             return 2
         else:
             return 3
