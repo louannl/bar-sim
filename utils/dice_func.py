0b9437e78bc0017324ea5d58ca81c0d4adf70cf0
nuran-o <nuran.a.ozan@gmail.com>
2021-12-08 23:05:16 +0000

    Update dice_func.py
    Refactoring DiceDecider class for unit tests.

--- utils/dice_func.py
+++ utils/dice_func.py
@@ -1,5 +1,3 @@
-# simple dice function
-
 import random
 
 
@@ -8,24 +6,20 @@ class DiceDecider:
     def __init__(self):
         self.dice_result = None
 
-    # self explanatory! its a simple dice
     def dice_roll(self):
-        self.dice_result = random.randint(1, 6)
-        return self.dice_result
+        dice_result = random.randint(1, 6)
+        return dice_result
 
-    # I got rid of 50/50 odds bc it was just this with more input options
-    def is_even(self):
-        self.dice_roll()
-        return (self.dice_result % 2) == 0
+    def is_even(self, num):
+        return (num % 2) == 0
 
-    # this is for the pint element!
-    def pint_increase_decider(self):
-        self.dice_roll()
-        if self.dice_result <= 2:
+    def pint_increase_decider(self, num):
+        if num <= 2:
             return 1
-        elif self.dice_result < 5:
+        elif num < 5:
             return 2
         else:
             return 3
 
-
+    def pint_increase(self):
+        return self.pint_increase_decider(self.dice_roll())
