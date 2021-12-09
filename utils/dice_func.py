73e71a58b52b19ebcb565d92869b11c05048258c
nuran-o <nuran.a.ozan@gmail.com>
2021-12-09 12:13:38 +0000

    Unit tests for dice decider function
    Dice decider function refactored slightly for purposes of unit testing

--- utils/dice_func.py
+++ utils/dice_func.py
@@ -10,7 +10,7 @@ class DiceDecider:
         dice_result = random.randint(1, 6)
         return dice_result
 
-    def is_even(self, num):
+    def is_even(self, num):  # odd_or_even
         return (num % 2) == 0
 
     def pint_increase_decider(self, num):
@@ -19,7 +19,16 @@ class DiceDecider:
         elif num < 5:
             return 2
         else:
-            return 3
+            return 3  # exit()
 
     def pint_increase(self):
         return self.pint_increase_decider(self.dice_roll())
+
+    def try_action(action):
+        if odd_or_even(dice_roll()):
+            print("Success! You have managed to {}".format(action))
+        else:
+            try_new_thing = input("Oh no! You failed to {}, would you like to try something else? y/n ".format(action))
+            if try_new_thing == 'y':
+                action = input("What would you like to try instead? ")
+                try_action(action)
