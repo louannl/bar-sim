00e49e06890e5b2e16f527248371cd65709f759b
nuran-o <nuran.a.ozan@gmail.com>
2021-12-11 17:45:35 +0000

    Update hero_class import
    Amends to api exceptions to correct this - will need more amends if we have time, to catch diff types of api errors-  get_character function particularly, atm does not catch if access_token/num not valid...

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,6 @@
 import os
+from sys import exit
+
 from dotenv import load_dotenv
 import requests
 
@@ -6,30 +8,26 @@ load_dotenv()
 
 
 def random_insult():
-    # moved URL so it exists outside of the try loop so that the except clause can access
     url = 'https://evilinsult.com/generate_insult.php?lang=en&type=json'
     try:
         request = requests.get(url).json()
         return request['insult']
-    # added exception so it's not a bare except clause
-    except Exception:
-        raise ConnectionError(f"Unable to connect to api {url}.")
+    except requests.exceptions.ConnectionError:
+        print(f"Unable to connect to api {url}.")
 
 
 def get_character(num):
     access_token = os.getenv('SUPERHERO_API_KEY')
-    url = f"https://superheroapi.com/api/{access_token}/{num}"
+    web_address = "https://superheroapi.com/api/"
+    url = f"{web_address}/{access_token}/{num}"
     try:
         return requests.get(url).json()
-    # same as above
-    except Exception:
-        raise ConnectionError(f"Unable to connect to api {url}.")
+    except requests.exceptions.ConnectionError:
+        print(f"Unable to connect to api {web_address}.")
 
 
 def set_user_character():
     # dict should maybe exist outside of the function, perhaps in the db?
-    # alternatively could we instead return a random selection of the superheroes? and have the user enter the
-    # corresponding id?
 
     player_options = {
         1: {'name': 'Buffy',
@@ -41,33 +39,27 @@ def set_user_character():
         4: {'name': 'Rambo',
             'id': 540}
     }
-    # this only displays the name - feels like a waste of the api! will api stats come into this?
+
     print("Which player would you like to select:")
     # can we display all player options at once? one at a time seems like poor UX
     for player_no, player_info in player_options.items():
-        player_select = input(f"Player {player_no}: {player_info['name']}?\nEnter y to accept or n to keep browsing:")
+        player_select = input(
+            f"Player {player_no}: {player_info['name']}?\nEnter 'y' to select or 'n' to keep browsing: ")
         if player_select == "y":
             confirmed_player_id = player_info["id"]
-            print("done")
             return confirmed_player_id
         if player_select == "n":
             pass
         else:
             print(
-                "I'm sorry that is not a recognised option. To select a player, you need to please press y or n. "
+                "I'm sorry that is not a recognised option. To select a player, you need to please enter 'y', to keep browsing enter 'n'. "
                 "Let's try again.")
             set_user_character()
     browse_again = input(
-        "You have browsed through all of our available player options. Do you want to try again? Enter y to select a "
-        "player or any key to exit the game.\n")
+        "You have browsed through all of our available player options. Do you want to try again? Enter 'y' to browse again or any key to exit the game.\n")
     if browse_again == "y":
+        print("Which player would you like to select: ")
         set_user_character()
     else:
         print("Thank you for playing Get Served.")
-        # I've made it an exit command as SystemExit came up with a warning that the statement did nothing
         exit()
-
-
-# this gave me {'response': 'error', 'error': 'access denied'}
-print(get_character(140))
-
