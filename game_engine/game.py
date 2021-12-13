6841596aa9484d4b402037b725cca491e0cc4c3d
Nuran Ozan <nuran.a.ozan@gmail.com>
2021-12-13 21:34:02 +0000

    Refactoring to code
    correction on dice_decider, adding bits for game

--- game_engine/game.py
+++ game_engine/game.py
@@ -1,12 +1,20 @@
+import random
+
+from game_engine.character import Character
+from utils.utils import get_character
+
+
 class Game:
-    def __init__(self) -> None:
+    def __init__(self, main_character: str) -> None:
         '''
         These variables are used when preparing the scenes, so make sure the
         name of the variable matches the story scenes when necessary i.e.
         [main_character] [superhero] etc.
         '''
-        self.main_character = 'Thomas The Tank Engine'
-        self.superhero = 'Deadpool'
+        main_character = main_character  # Character(get_character(set_user_character()))  #BUT THIS WILL ASK USER FOR INPUTS
+        superhero = Character(get_character(str(random.randint(1, 731))))
+        self.main_character = main_character.getName()
+        self.superhero = superhero.getName()
         self.prize = 'Stella Artois'
         self.pints = 0
 
