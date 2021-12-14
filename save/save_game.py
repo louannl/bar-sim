b63e26dca30f4b3b31f525967123241157b2b9df
louannl <louann_loizou@hotmail.com>
2021-12-14 13:37:27 +0000

    added update game save query, needs some amending

--- save/save_game.py
+++ save/save_game.py
@@ -23,7 +23,6 @@ class CreateGame:
 
     def create_game_save(self, player_id: int, character: str, won: bool):
         game_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        print('date: ', game_date)
 
         query_string = '''
         INSERT INTO game (player_id, player_character, won, game_date)
@@ -31,4 +30,19 @@ class CreateGame:
         '''
         send_game_connection = self.query.db_connect(
             query_string, [player_id, character, won, game_date])
+        return send_game_connection
+
+
+class UpdateGame:
+    def __init__(self, query: Query) -> None:
+        self.query = query
+
+    def update_game_save(self, player_id: int):
+        query_string = '''
+        UPDATE player
+        SET total_plays = total_plays + 1
+        WHERE id = %s;
+        '''
+        send_game_connection = self.query.db_connect(
+            query_string, [player_id])
         return send_game_connection
