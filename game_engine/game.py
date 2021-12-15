3defe0d0d050c597329fecc6ef52843be689c06b
Nuran Ozan <nuran.a.ozan@gmail.com>
2021-12-15 20:15:57 +0000

    Refactor to game file
    I have added calls to instantiate the Character class (which also calls functions get character and set user). Example calls at the bottom. You can access the Character class methods through these calls.
    
    I have also entered a range of other super heroes that can be randomly selected from as we spoke about in last call.
    
    I know one of Zoe's comments was that this player options etc, should be stored somewhere more tidily, maybe in a database. I guess we could put that as a suggestion for improvement/expansion as doubt we'll get to it otherwise?

--- game_engine/game.py
+++ game_engine/game.py
@@ -1,3 +1,9 @@
+import random
+
+from utils.utils import set_user_character, get_character
+from game_engine.character import Character
+
+
 class Game:
     def __init__(self) -> None:
         '''
@@ -5,10 +11,21 @@ class Game:
         name of the variable matches the story scenes when necessary i.e.
         [main_character] [superhero] etc.
         '''
-        self.main_character = 'Thomas The Tank Engine'
-        self.superhero = 'Deadpool'
+        self.superhero = Character(get_character(random.choice(list({60: "Bane", 69:"Batman", 97: "Black Canary", 309: "Harlequin", 322: "Hellboy", 522:"Poison Ivy", 374: "Juggernaut", 400: "Lady Deathstrike", 489: "Nick Fury"}))))
         self.prize = 'Stella Artois'
         self.pints = 0
 
-    def update_main_character(self, character: str) -> None:
-        self.main_character = character
+    def update_main_character(self) -> None:
+        self.main_character = Character(get_character(set_user_character())) #Will prompt user for inputs
+
+
+# Example calls to get other superhero name and main_character name:
+#accesses methods from Characters class
+#
+# example = Game()
+#
+# print(example.superhero.name)
+#
+# example.update_main_character()
+# print(example.main_character.name)
+
