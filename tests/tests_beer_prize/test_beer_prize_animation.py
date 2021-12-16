5f2d0fec01f01935d32ec96cc4e67bb172c83a7c
SunithaWebster <sunithaweb@yahoo.co.uk>
2021-12-16 15:26:10 +0000

    Tests for beer prize animation.

---
+++ tests/tests_beer_prize/test_beer_prize_animation.py
@@ -0,0 +1,21 @@
+import unittest
+import beer_prize_animation
+from beer_prize_animation import BeerPourSprite
+
+path: str = r"\bar-sim-main\tests\tests_beer_prize\test_beer_prize_images"
+
+
+class TestImageLoading(unittest.TestCase):
+    def test_images_load(self):
+        result = beer_prize_animation.load_images()
+        self.assertTrue(result)
+
+
+class TestBeerPourSprite(unittest.TestCase):
+    def test_sprite_instantiates(self):
+        result = BeerPourSprite(position=(10, 10), images=path)
+        self.assertTrue(result)
+
+
+if __name__ == '__main__':
+    unittest.main()
