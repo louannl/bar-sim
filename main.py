f123aeb160fd1467bdf797469eb8df94cadeeca0
Zoe Thomas <zoethomas2893@gmail.com>
2021-12-16 20:40:20 +0000

    "added a leaderboard"

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import json
 from os import error
 from beer_prize.beer_prize_animation import play_beer
-
 from game_engine.game import Game
 from game_engine.scene import Scene, scene_generator
 from utils.save_helpers import create_or_return_player_id, return_play_and_win_count, save_game
+from save.save_game import GetGameHistory
+from save.query import Query
 
 
 with open("story/scenes.json") as jsonScenesFile:
@@ -29,4 +30,6 @@ save_game(game_state)
 wins, plays = return_play_and_win_count(game_state)
 print(
     f'You have played {plays} time{"s" if plays > 1 else ""}, and won {wins} time{"s" if wins > 1 else ""}')
+lb = GetGameHistory(Query())
+print(lb.display_leaderboard())
 play_beer()
