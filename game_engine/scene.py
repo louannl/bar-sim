f5d8b1e64e782f5a14e8415dfba8fceaa301519d
louannl <louann_loizou@hotmail.com>
2021-12-17 16:43:28 +0000

    added timers to delay earthquake scene switches

--- game_engine/scene.py
+++ game_engine/scene.py
@@ -3,6 +3,7 @@ from game_engine.character import Character
 from game_engine.game import Game
 from utils.dice_decider import DiceDecider
 import webbrowser
+import time
 
 
 class Scene:
@@ -58,7 +59,7 @@ def scene_generator(scene: Scene, game_state: Game) -> str:
     print(scene.render_result(user_choice))
 
     scene_choice = scene.options[str(user_choice)]
-    if scene_choice['choice'] == 'Super Duper Powerful Combo Attack':
+    if scene_choice['choice'] == 'Ask Rick':
         webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
         return 'lose'
 
@@ -74,38 +75,54 @@ def scene_generator(scene: Scene, game_state: Game) -> str:
         game_state.victory()
 
     if next_scene == 'earthquake':
+        time.sleep(3)
+        print("The murky flagstones cleave and fracture as you abandon all thoughts of pints or bars and simply play hopscotch at the end of days.\nThe earth itself seems to breaking into a grin, presenting a gaping crevasse that prepares for your descent, but before you slip into it, someone grabs your legs and hauls you back, under a table.\nIt's the other barkeep.  You hope that your deep appreciation can be read from your eyes.\nBefore you can speak, an aftershock thrusts you together, while rubble and debris engulf the table above.  You're sealed in.\n So. This is how it ends.\nYou cling to each other.")
+        time.sleep(3)
         select = randint(1, 2)
         if select == 1:
+            print('You attempt to push the rubble to break free')
             return strength_scene_test(game_state)
+        print('There\'s only one way to survive the rubble... Roll-a-dice..')
         return dice_scene(game_state)
 
     return next_scene
 
 
 def dice_scene(game_state: Game) -> str:
+    time.sleep(1)
     print('You chuck the dice high, it falls...')
+    time.sleep(5)
     dice_decider = DiceDecider()
     roll_amount = dice_decider.dice_roll()
     print(f'You rolled a {roll_amount}')
     if roll_amount > 3:
+        time.sleep(1)
         print('Success!')
+        time.sleep(3)
         game_state.update_pints(2)
         return 'win'
+    time.sleep(1)
     print('oh no... how unlucky...')
     game_state.update_pints(-2)
+    time.sleep(3)
     return 'lose'
 
 
 def strength_scene_test(game_state: Game) -> str:
     print('You push with all your might!')
+    time.sleep(5)
     char_str = game_state.main_character.strength
     print(f'Your chosen character\'s strength is: {char_str}')
     if int(char_str) > 30:
         print('You push it way, and save yourself and the barman! \nAs you struggle for the door, the barman grabs a pint for you...')
+        time.sleep(3)
         game_state.update_pints(2)
+        time.sleep(1)
         return 'win'
+    time.sleep(5)
     print('You\'re not strong enough, you\'re pushed back by the force...Oh no...')
     game_state.update_pints(-2)
+    time.sleep(3)
     return 'lose'
 
 
