c281c50fd286bfd53fbf4289fa72b1ef51a45097
nuran-o <nuran.a.ozan@gmail.com>
2021-12-18 12:41:03 +0000

    Fight class updated to get it working

--- utils/fight.py
+++ utils/fight.py
@@ -6,12 +6,12 @@ from game_engine.character import Character
 class FightClub:
 
     def __init__(self, superhero: Character, main_character: Character) -> None:
+        self.superhero = superhero
+        self.main_character = main_character
         self.main_char_win_speed = superhero.speed < main_character.speed
         self.main_char_win_strength = superhero.strength < main_character.strength
         self.main_char_win_intelligence = superhero.intelligence > main_character.intelligence
         self.char_wins = 0
-        self.superhero = Character
-
 
     def round1_speed(self):
         if self.main_char_win_speed:
@@ -22,12 +22,11 @@ class FightClub:
         time.sleep(3)
         return True
 
-
     def round2_intelligence(self):
         if self.main_char_win_intelligence:
             print(f"In the background, you can hear someone just put an 80s banger on the pub jukebox. You start singing like your life depends on it 'It's the eye of the tiger, it's the thrill of the fight!'")
             time.sleep(5)
-            print(f"Your rousing soprano voice elicits a hearty chorus back from drunken pub-goers. People start hugging.  You use {self.superhero.name}'s confusion as a chance to gather your breath!")
+            print(f"Your rousing soprano voice elicits a hearty chorus back from drunken pub-goers. People start hugging. You use {self.superhero.name}'s confusion as a chance to gather your breath!")
             time.sleep(5)
             self.char_wins += 1
         else:
@@ -36,19 +35,16 @@ class FightClub:
             print('*Crickets*. You get a mildly annoyed look in response - "Wow. You expected me to fall for that?"')
             time.sleep(5)
 
-
     def narration(self):
         print(f"Your opponent is coming towards you looking angry! You stumble a few steps backwards but there is no where to run...")
         time.sleep(5)
         print(f"Your opponent is picking up speed and yells araaaaaaaaaaghghhhhh as they go in to tackle you.")
         time.sleep(5)
-        print(
-            f"There's nothing else for it - you lower your head and start running towards {self.superhero.name} - you shout aiaiaaiaiaiaiiiiiiiiii!")
+        print(f"There's nothing else for it - you lower your head and start running towards {self.superhero.name} - you shout aiaiaaiaiaiaiiiiiiiiii!")
         print(f"It's a terrifying game of chicken!")
         time.sleep(5)
         print(f"There's an almighty crunch of skull hitting and skull. A silence comes across the pub...")
         time.sleep(6)
-
 
     def round3_strength_and_outcome(self) -> bool:
         if self.main_char_win_strength:
@@ -58,9 +54,8 @@ class FightClub:
             print(f"Ooph, you find yourself flat on your back with tiny birds floating around your head. Strange, were they there earlier? You get onto your feet staggering slightly")
         return self.char_wins >= 2
 
-
     def activate_rounds_get_outcome(self):
-        round1_speed()
-        round2_intelligence()
-        narration()
-        round3_strength_and_outcome()
+        self.round1_speed()
+        self.round2_intelligence()
+        self.narration()
+        return self.round3_strength_and_outcome()
