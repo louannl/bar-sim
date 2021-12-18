3b617607a574d1e3c382ca630f5fadd4683a4291
nuran-o <nuran.a.ozan@gmail.com>
2021-12-18 10:43:25 +0000

    Updated to incorporate changes (indentation and remove brackets from class def)
    Updated to incorporate changes (indentation and remove brackets from class def)

--- utils/fight.py
+++ utils/fight.py
@@ -3,70 +3,64 @@ import time
 from game_engine.character import Character
 
 
-class FightClub():
+class FightClub:
+
+    def __init__(self, superhero: Character, main_character: Character) -> None:
+        self.main_char_win_speed = superhero.speed < main_character.speed
+        self.main_char_win_strength = superhero.strength < main_character.strength
+        self.main_char_win_intelligence = superhero.intelligence > main_character.intelligence
+        self.char_wins = 0
+        self.superhero = Character
 
 
-def __init__(self, superhero: Character, main_character: Character) -> None:
-    self.main_char_win_speed = superhero.speed < main_character.speed
-    self.main_char_win_strength = superhero.strength < main_character.strength
-    self.main_char_win_intelligence = superhero.intelligence > main_character.intelligence
-    self.char_wins = 0
-    self.superhero = Character
+    def round1_speed(self):
+        if self.main_char_win_speed:
+            print(f"Nice one! Your punch makes contact with a satisfying smack!")
+            char_wins += 1
+        else:
+            print(f"{superhero.name} dodges it and your fist goes crashing into the wall - ouch!")
+        time.sleep(3)
+        return True
 
 
-def round1_speed(self):
-    if self.main_char_win_speed:
-        print(f"Nice one! Your punch makes contact with a satisfying smack!")
-        char_wins += 1
-    else:
-        print(f"{superhero.name} dodges it and your fist goes crashing into the wall - ouch!")
-    time.sleep(3)
-    return True
+    def round2_intelligence(self):
+        if self.main_char_win_intelligence:
+            print(f"In the background, you can hear someone just put an 80s banger on the pub jukebox. You start singing like your life depends on it 'It's the eye of the tiger, it's the thrill of the fight!'")
+            time.sleep(5)
+            print(f"Your rousing soprano voice elicits a hearty chorus back from drunken pub-goers. People start hugging.  You use {superhero.name}'s confusion as a chance to gather your breath!")
+            time.sleep(5)
+            self.char_wins += 1
+        else:
+            print(f'''You try to distract {superhero.name} - "hey, what's that over there?!"''')
+            time.sleep(5)
+            print('*Crickets*. You get a mildly annoyed look in response - "Wow. You expected me to fall for that?"')
+            time.sleep(5)
 
 
-def round2_intelligence(self):
-    if self.main_char_win_intelligence:
-        print(
-            f"In the background, you can hear someone just put an 80s banger on the pub jukebox. You start singing like your life depends on it 'It's the eye of the tiger, it's the thrill of the fight!'")
+    def narration(self):
+        print(f"Your opponent is coming towards you looking angry! You stumble a few steps backwards but there is no where to run...")
+        time.sleep(5)
+        print(f"Your opponent is picking up speed and yells araaaaaaaaaaghghhhhh as they go in to tackle you.")
         time.sleep(5)
         print(
-            f"Your rousing soprano voice elicits a hearty chorus back from drunken pub-goers. People start hugging.  You use {superhero.name}'s confusion as a chance to gather your breath!")
+            f"There's nothing else for it - you lower your head and start running towards {superhero.name} - you shout aiaiaaiaiaiaiiiiiiiiii!")
+        print(f"It's a terrifying game of chicken!")
         time.sleep(5)
-        self.char_wins += 1
-    else:
-        print(f'''You try to distract {superhero.name} - "hey, what's that over there?!"''')
-        time.sleep(5)
-        print('*Crickets*. You get a mildly annoyed look in response - "Wow. You expected me to fall for that?"')
-        time.sleep(5)
+        print(f"There's an almighty crunch of skull hitting and skull. A silence comes across the pub...")
+        time.sleep(6)
 
 
-def narration(self):
-    print(
-        f"Your opponent is coming towards you looking angry! You stumble a few steps backwards but there is no where to run...")
-    time.sleep(5)
-    print(f"Your opponent is picking up speed and yells araaaaaaaaaaghghhhhh as they go in to tackle you.")
-    time.sleep(5)
-    print(
-        f"There's nothing else for it - you lower your head and start running towards {superhero.name} - you shout aiaiaaiaiaiaiiiiiiiiii!")
-    print(f"It's a terrifying game of chicken!")
-    time.sleep(5)
-    print(f"There's an almighty crunch of skull hitting and skull. A silence comes across the pub...")
-    time.sleep(6)
+    def round3_strength_and_outcome(self) -> bool:
+        if self.main_char_win_strength:
+            print(f"You look around and are relieved to see {superhero.name} out cold on the floor. You drag your worthy foe to a nearby stool and prop him up in it. His head will hurt when he wakes up.")
+            self.char_wins += 1
+        else:
+            print(f"Ooph, you find yourself flat on your back with tiny birds floating around your head. Strange, were they there earlier? You get onto your feet staggering slightly")
+        return self.char_wins >= 2
 
 
-def round3_strength_and_outcome(self) -> bool:
-    if self.main_char_win_strength:
-        print(
-            f"You look around and are relieved to see {superhero.name} out cold on the floor. You drag your worthy foe to a nearby stool and prop him up in it. His head will hurt when he wakes up.")
-        self.char_wins += 1
-    else:
-        print(
-            f"Ooph, you find yourself flat on your back with tiny birds floating around your head. Strange, were they there earlier? You get onto your feet staggering slightly")
-    return self.char_wins >= 2
-
-
-def activate_rounds_get_outcome(self):
-    round1_speed()
-    round2_intelligence()
-    narration()
-    round3_strength_and_outcome()
+    def activate_rounds_get_outcome(self):
+        round1_speed()
+        round2_intelligence()
+        narration()
+        round3_strength_and_outcome()
