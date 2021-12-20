b7c699c006b7de10e6735bc3be7ca66fae51bde0
louannl <louann_loizou@hotmail.com>
2021-12-20 13:18:30 +0000

    added tests to file names, fixed imports on test files, amended game tests

--- tests/unit/utils_tests/test_utils.py
+++ tests/unit/utils_tests/test_utils.py
@@ -1,17 +1,20 @@
 from unittest import TestCase, main
-from utils.utils import random_insult, get_character, set_user_character
+from utils.utils import random_insult, get_character
 from utils.fight import FightClub
-from tests.game_engine.helper import dummy_character_request, dummy_character_request_win_stats
+from tests.unit.game_engine.helper import dummy_character_request, dummy_character_request_win_stats
 
 
 class RandomInsult(TestCase):
 
     def is_api_response200(self):
-        self.assertTrue("<Response[200]>", random_insult())
+        insult = random_insult()
+        self.assertTrue("<Response[200]>", insult)
 
     def is_str_returned_not_empty(self):
-        self.assertTrue(str, random_insult())
-        self.assertTrue(random_insult())  # an empty string would evaluate False
+        insult = random_insult()
+        self.assertTrue(str, insult)
+        # an empty string would evaluate False
+        self.assertTrue(insult)
 
     def is_api_exception_raised(self):
         self.assertRaises(Exception, random_insult())
@@ -37,41 +40,49 @@ class GetCharacter(TestCase):
         self.assertTrue("<Response[200]>", get_character(522))
 
     def is_api_exception_raised(self):
-        self.assertFalse(Exception, get_character(
-            1))  # If the url works fine, this test would break so changed to False. So if Url not working, then it breaks
+        # If the url works fine, this test would break so changed to False. So if Url not working, then it breaks
+        self.assertFalse(Exception, get_character(1))
 
 
 class FightClub(TestCase):
     def r1_main_char_win_speed(self):
-        test_call = FightClub(dummy_character_request_win_stats, dummy_character_request)
+        test_call = FightClub(
+            dummy_character_request_win_stats, dummy_character_request)
         self.assertTrue(test_call.round1_speed())
 
     def r2_main_char_win_intelligence(self):
-        test_call = FightClub(dummy_character_request_win_stats, dummy_character_request)
+        test_call = FightClub(
+            dummy_character_request_win_stats, dummy_character_request)
         self.assertTrue(test_call.round2_intelligence())
 
     def r3_main_char_win_strength(self):
-        test_call = FightClub(dummy_character_request_win_stats, dummy_character_request)
+        test_call = FightClub(
+            dummy_character_request_win_stats, dummy_character_request)
         self.assertTrue(test_call.round3_strength())
 
     def activate_rounds_outcome_main_char_win(self):
-        test_call = FightClub(dummy_character_request_win_stats, dummy_character_request)
+        test_call = FightClub(
+            dummy_character_request_win_stats, dummy_character_request)
         self.assertTrue(test_call.activate_rounds_get_outcome())
 
     def r1_main_char_lose_speed_(self):
-        test_call = FightClub(dummy_character_request, dummy_character_request_win_stats)
+        test_call = FightClub(dummy_character_request,
+                              dummy_character_request_win_stats)
         self.assertTrue(test_call.round1_speed())
 
     def r2_main_char_lose_intelligence(self):
-        test_call = FightClub(dummy_character_request, dummy_character_request_win_stats)
+        test_call = FightClub(dummy_character_request,
+                              dummy_character_request_win_stats)
         self.assertTrue(test_call.round2_intelligence())
 
     def r3_main_char_lose_strength(self):
-        test_call = FightClub(dummy_character_request, dummy_character_request_win_stats)
+        test_call = FightClub(dummy_character_request,
+                              dummy_character_request_win_stats)
         self.assertTrue(test_call.round3_strength())
 
     def activate_rounds_outcome_main_char_lose(self):
-        test_call = FightClub(dummy_character_request, dummy_character_request_win_stats)
+        test_call = FightClub(dummy_character_request,
+                              dummy_character_request_win_stats)
         self.assertTrue(test_call.activate_rounds_get_outcome())
 
 
