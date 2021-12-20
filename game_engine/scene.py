1fddf79890c6c8344f089c172cf02dd1e9276b58
louannl <louann_loizou@hotmail.com>
2021-12-20 19:02:24 +0000

    added pint change to fight scene

--- game_engine/scene.py
+++ game_engine/scene.py
@@ -137,7 +137,9 @@ def fight_scene(game_state: Game) -> str:
     fight_club = FightClub(game_state.superhero, game_state.main_character)
     fight_result = fight_club.activate_rounds_get_outcome()
     if fight_result:
+        game_state.update_pints(2)
         return 'win'
+    game_state.update_pints(-2)
     return 'fightTime'
 
 
