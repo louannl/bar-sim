d0703b70dfca5eb4d51435647ac6c37d17efea02
louannl <louann_loizou@hotmail.com>
2021-12-20 19:33:28 +0000

    moved earthquake scene out of scene generator

--- game_engine/scene.py
+++ game_engine/scene.py
@@ -82,17 +82,21 @@ def scene_generator(scene: Scene, game_state: Game) -> str:
         game_state.victory()
 
     if next_scene == 'earthquake':
-        time.sleep(3)
-        print("The murky flagstones cleave and fracture as you abandon all thoughts of pints or bars and simply play hopscotch at the end of days.\nThe earth itself seems to breaking into a grin, presenting a gaping crevasse that prepares for your descent, but before you slip into it, someone grabs your legs and hauls you back, under a table.\nIt's the other barkeep.  You hope that your deep appreciation can be read from your eyes.\nBefore you can speak, an aftershock thrusts you together, while rubble and debris engulf the table above.  You're sealed in.\n So. This is how it ends.\nYou cling to each other.")
-        time.sleep(3)
-        select = randint(1, 2)
-        if select == 1:
-            print('You attempt to push the rubble to break free')
-            return strength_scene_test(game_state)
-        print('There\'s only one way to survive the rubble... Roll-a-dice..')
-        return dice_scene(game_state)
+        return earthquake_scene()
 
     return next_scene
+
+
+def earthquake_scene(game_state: Game) -> None:
+    time.sleep(3)
+    print("The murky flagstones cleave and fracture as you abandon all thoughts of pints or bars and simply play hopscotch at the end of days.\nThe earth itself seems to breaking into a grin, presenting a gaping crevasse that prepares for your descent, but before you slip into it, someone grabs your legs and hauls you back, under a table.\nIt's the other barkeep.  You hope that your deep appreciation can be read from your eyes.\nBefore you can speak, an aftershock thrusts you together, while rubble and debris engulf the table above.  You're sealed in.\n So. This is how it ends.\nYou cling to each other.")
+    time.sleep(3)
+    choice = randint(1, 2)
+    if choice == 1:
+        print('You attempt to push the rubble to break free')
+        return strength_scene_test(game_state)
+    print('There\'s only one way to survive the rubble... Roll-a-dice..')
+    return dice_scene(game_state)
 
 
 def dice_scene(game_state: Game) -> str:
