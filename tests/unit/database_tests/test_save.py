e06b19c29b44c267becdbbd8844f7fcf9698e5be
louannl <louann_loizou@hotmail.com>
2021-12-21 11:51:20 +0000

    amended game class tests

--- tests/unit/database_tests/test_save.py
+++ tests/unit/database_tests/test_save.py
@@ -1,11 +1,11 @@
-import unittest
+from unittest import TestCase, main
 from unittest.mock import patch, MagicMock
 import save.db_connection
-from save.db_connection import Query
+from save.db_connection import DBConnection
 
 
-@patch('save.query.connect_db')
-class MyTestCase(unittest.TestCase):
+@patch('save.db_connection.connect_db')
+class MyTestCase(TestCase):
 
     def test_connection_patch(self, connect_db):
         self.assertIs(save.db_connection.connect_db, connect_db)
@@ -14,74 +14,74 @@ class MyTestCase(unittest.TestCase):
         mock_conn = MagicMock()
         connect_db.return_value = mock_conn
         cursor = mock_conn.cursor.return_value
-        query_obj = Query()
+        query_obj = DBConnection()
         player_name = 'player name'
         query = '''SELECT * FROM player WHERE full_name = %s'''
-        query_obj.db_connect(query, [player_name])
+        query_obj.query(query, [player_name])
 
         cursor.execute.assert_called_with(query, [player_name])
         self.assertEqual(1, cursor.close.call_count)
         self.assertEqual(1, cursor.fetchall.call_count)
         self.assertEqual(1, mock_conn.commit.call_count)
         self.assertEqual(1, mock_conn.close.call_count)
-        self.assertRaises(Exception, query_obj.db_connect,
+        self.assertRaises(Exception, query_obj.query,
                           query, player_id=' ')
 
     def test_create_player_query(self, connect_db):
         mock_conn = MagicMock()
         connect_db.return_value = mock_conn
         cursor = mock_conn.cursor.return_value
-        query_obj = Query()
+        query_obj = DBConnection()
         player_name = 'player name'
         query = '''INSERT INTO player (full_name) VALUES (%s);'''
-        query_obj.db_connect(query, [player_name])
+        query_obj.query(query, [player_name])
 
         cursor.execute.assert_called_with(query, [player_name])
         self.assertEqual(1, cursor.close.call_count)
         self.assertEqual(1, cursor.fetchall.call_count)
         self.assertEqual(1, mock_conn.commit.call_count)
         self.assertEqual(1, mock_conn.close.call_count)
-        self.assertRaises(Exception, query_obj.db_connect,
+        self.assertRaises(Exception, query_obj.query,
                           query, player_id=' ')
 
     def test_return_play_count_and_win_count_query(self, connect_db):
         mock_conn = MagicMock()
         connect_db.return_value = mock_conn
         cursor = mock_conn.cursor.return_value
-        query_obj = Query()
+        query_obj = DBConnection()
         player_id = 'player_id'
         query = '''SELECT count(case when won = 1 then 1 end), count(*)
         FROM game
         WHERE player_id = %s'''
-        query_obj.db_connect(query, [player_id])
+        query_obj.query(query, [player_id])
 
         cursor.execute.assert_called_with(query, [player_id])
         self.assertEqual(1, cursor.close.call_count)
         self.assertEqual(1, cursor.fetchall.call_count)
         self.assertEqual(1, mock_conn.commit.call_count)
         self.assertEqual(1, mock_conn.close.call_count)
-        self.assertRaises(Exception, query_obj.db_connect,
+        self.assertRaises(Exception, query_obj.query,
                           query, player_name=' ')
 
     def test_display_leaderboard_connection(self, connect_db):
         mock_conn = MagicMock()
         connect_db.return_value = mock_conn
         cursor = mock_conn.cursor.return_value
-        query_obj = Query()
+        query_obj = DBConnection()
         query = '''SELECT p.full_name AS player, sum(pint_count) AS pint_total
         FROM game AS g
         JOIN player AS p on p.id = g.player_id
         GROUP BY 1
         ORDER BY 2 desc
         LIMIT 3;'''
-        query_obj.db_connect_to_table(query)
+        query_obj.query_table(query)
 
         cursor.execute.assert_called_with(query)
         self.assertEqual(1, cursor.close.call_count)
         self.assertEqual(1, cursor.fetchall.call_count)
         self.assertEqual(1, mock_conn.commit.call_count)
         self.assertEqual(1, mock_conn.close.call_count)
-        self.assertRaises(Exception, query_obj.db_connect,
+        self.assertRaises(Exception, query_obj.query,
                           query, player_id=' ')
 
     @patch('prettytable.from_db_cursor')
@@ -89,37 +89,37 @@ class MyTestCase(unittest.TestCase):
         mock_conn = MagicMock()
         connect_db.return_value = mock_conn
         cursor = mock_conn.cursor.return_value
-        query_obj = Query()
+        query_obj = DBConnection()
         query = '''SELECT p.full_name AS player, sum(pint_count) AS pint_total
             FROM game AS g
             JOIN player AS p on p.id = g.player_id
             GROUP BY 1
             ORDER BY 2 desc
             LIMIT 3;'''
-        query_obj.db_connect_to_table(query)
+        query_obj.query_table(query)
         self.assertTrue(from_db_cursor.called)
 
     def test_create_game_save_query(self, connect_db):
         mock_conn = MagicMock()
         connect_db.return_value = mock_conn
         cursor = mock_conn.cursor.return_value
-        query_obj = Query()
+        query_obj = DBConnection()
         player_id, character, won, pint_count, game_date = 'player_id', 'char', 'Bool', 'count', 'time_stamp'
         params = [player_id, character, won, pint_count, game_date]
         query = '''
         INSERT INTO game (player_id, player_character, won, pint_count, game_date)
         VALUES (%s, %s, %s, %s, %s)
         '''
-        query_obj.db_connect(query, params)
+        query_obj.query(query, params)
 
         cursor.execute.assert_called_with(query, params)
         self.assertEqual(1, cursor.close.call_count)
         self.assertEqual(1, cursor.fetchall.call_count)
         self.assertEqual(1, mock_conn.commit.call_count)
         self.assertEqual(1, mock_conn.close.call_count)
-        self.assertRaises(Exception, query_obj.db_connect,
+        self.assertRaises(Exception, query_obj.query,
                           query, player_id=' ')
 
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
--- tests/unit/game_engine_tests/game_tests/test_game.py
+++ tests/unit/game_engine_tests/game_tests/test_game.py
@@ -3,39 +3,54 @@ from unittest.mock import patch
 
 from game_engine.character.character import Character
 from game_engine.game.game import Game
+from game_engine.game.pints_counter import PintCounter
+from game_engine.scene.scene_manager import SceneManager
+from save.end import End
 from save.player import Player
-from save.db_connection import Query
-from tests.unit.game_engine_tests.dummy_data import dummy_character_request
+from save.db_connection import DBConnection
+from save.save import Save
+from save.stats import Stats
+from tests.unit.game_engine_tests.dummy_data import (
+    dummy_character_request, dummy_character_request_win_stats)
 
 
 class GameTestCase(TestCase):
     def test_update_character(self):
+        db = DBConnection()
         game_class = Game(
+            Player(db, 'Thomas the Tank Engine'),
             Character(dummy_character_request),
             Character(dummy_character_request),
-            Player(Query(), 'lou')
+            SceneManager(),
+            PintCounter(),
+            Save(db),
+            End(Stats(db))
         )
-        game_class.update_main_character('David')
-        self.assertEqual('David', game_class.main_character)
-
-    def test_get_pints_returns_num(self):
-        game_class = Game(
-            Character(dummy_character_request),
-            Character(dummy_character_request),
-            Player(Query(), 'lou')
+        game_class.update_main_character(dummy_character_request_win_stats)
+        self.assertEqual(
+            dummy_character_request_win_stats,
+            game_class.main_character
         )
-        result = game_class.get_pints()
-        self.assertEqual(9, result)
 
     @patch('random.random')
     def test_get_random_beer_random_choice_called(self, mock_random):
+        db = DBConnection()
         game_class = Game(
+            Player(db, 'Thomas the Tank Engine'),
             Character(dummy_character_request),
             Character(dummy_character_request),
-            Player(Query(), 'lou')
+            SceneManager(),
+            PintCounter(),
+            Save(db),
+            End(Stats(db))
         )
         game_class.get_random_beer()
         self.assertTrue(1, mock_random.choice.call_count)
+
+    def test_get_pints_returns_starting_pints(self):
+        pint_counter = PintCounter()
+        result = pint_counter.get_pints()
+        self.assertEqual(9, result)
 
 
 if __name__ == '__main__':
